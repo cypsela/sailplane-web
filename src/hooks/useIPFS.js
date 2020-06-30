364cb4f101c6f17b2957dafc757b218f235fd70d
Steve Dakh <slavik0329@gmail.com>
2020-06-30 17:29:53 -0400

    useIPFS style ;)

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -23,10 +23,10 @@ export default function useIpfsFactory() {
                 Swarm: [
                   '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
                   '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-                  '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/'
-                ]
-              }
-            }
+                  '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
+                ],
+              },
+            },
           });
           console.timeEnd('IPFS Started');
         } catch (error) {
@@ -52,4 +52,3 @@ export default function useIpfsFactory() {
 
   return {ipfs, isIpfsReady, ipfsInitError};
 }
-
