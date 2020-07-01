005edc38eade955c221f6bdcf1865b77e2666078
Steve Dakh <slavik0329@gmail.com>
2020-07-01 17:50:38 -0400

    audio previews

--- src/components/FilePreview.js
+++ src/components/FilePreview.js
@@ -3,8 +3,12 @@ import React from 'react';
 const styles = {
   image: {
     width: 300,
+  },
+  audio: {
+    userSelect: 'none',
+    outline: 0,
   }
-}
+};
 
 export function FilePreview({blob, filename}) {
   console.log('blo', blob, filename);
@@ -13,10 +17,17 @@ export function FilePreview({blob, filename}) {
   const objURL = window.URL.createObjectURL(blob);
 
   return (
-    <div onClick={(event)=>event.stopPropagation()}>
+    <div onClick={(event) => event.stopPropagation()}>
       {['jpg', 'jpeg', 'png', 'gif'].includes(ext) ? (
         <div>
-          <img src={objURL} style={styles.image}/>
+          <img src={objURL} style={styles.image} />
+        </div>
+      ) : null}
+      {['mp3', 'wav', 'ogg', 'flac'].includes(ext) ? (
+        <div>
+          <audio controls>
+            <source src={objURL} style={styles.audio}/>
+          </audio>
         </div>
       ) : null}
     </div>
