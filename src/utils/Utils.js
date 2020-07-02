b53dfbef9498f37f841134bc7fe388da978ad2f4
anders <andersb@tuta.io>
2020-07-02 03:35:24 -0500

    close to folder downloads

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,10 +1,53 @@
-const first = require('it-first');
-const all = require('it-all');
+
+import first from 'it-first'
+import all from 'it-all'
+import JSZip from 'jszip'
+
+async function fileToBlob({ content }) {
+  return new Blob(await all(content))
+}
+
+async function dirToBlob(path, struct) {
+  const zip = new JSZip()
+  const root = path.split('/')[path.split('/').length - 1]
+
+  await Promise.all([
+    struct.map(async (item) => {
+      const isDir = item.type === 'dir'
+
+      const splitPath = item.path.split('/')
+      splitPath[0] = root
+      const path = splitPath.join('/')
+      const blob = isDir ? undefined : await fileToBlob(item)
+
+      console.log(path)
+      console.log(blob)
+
+      zip.file(path, blob, { dir: isDir })
+    })
+  ])
+
+  const data = await zip.generateAsync({ type: 'blob' })
+  console.log(data)
+  return data
+}
 
 export async function getBlobFromPath(sharedFs, path, ipfs) {
   const cid = await sharedFs.current.read(path);
-  const file = await first(ipfs.get(cid));
-  const fileContent = await all(file.content);
-  const blob = new Blob(fileContent);
-  return blob;
+  const struct = await all(ipfs.get(cid));
+
+  return struct[0].type === 'dir'
+    ? dirToBlob(path, struct)
+    : fileToBlob(struct[0])
 }
+
+
+
+
+// export async function getBlobFromPath(sharedFs, path, ipfs) {
+//   const cid = await sharedFs.current.read(path);
+//   const file = await first(ipfs.get(cid));
+//   const fileContent = await all(file.content);
+//   const blob = new Blob(fileContent);
+//   return blob;
+// }
