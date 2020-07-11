6244a79a18b662713c8c098ddd49469e5ff4f343
Steve Dakh <slavik0329@gmail.com>
2020-07-10 22:26:05 -0400

    Revert "use private signal server"
    This reverts commit da3bbedc52052ab287d68d6461626a3078ae3eaa.

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -24,7 +24,10 @@ export default function useIpfsFactory() {
             config: {
               Addresses: {
                 Swarm: [
-                  '/dns4/jotcube.com/tcp/9090/ws/p2p-webrtc-star/',
+                  '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+                  '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+                  '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
+                  '/dns4/libp2p-rdv.vps.revolunet.com/tcp/443/wss/p2p-webrtc-star/',
                 ],
               },
             },
