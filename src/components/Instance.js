590c28a814fbe1855e209eb4f7835cfdb45c8138
Steve Dakh <slavik0329@gmail.com>
2020-07-19 19:44:34 -0400

    instance redesign

--- src/components/Instance.js
+++ src/components/Instance.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import {errorColor, primary, primary2, primary3, primary45} from '../colors';
-import useHover from '../hooks/useHover';
 import {ToolItem} from './ToolItem';
 import {FiShare2, FiCopy, FiTrash} from 'react-icons/fi';
-import {useElementCopy} from '../hooks/useElementCopy';
-import {FiHardDrive, FiUsers} from 'react-icons/fi/index';
+import {FiHardDrive} from 'react-icons/fi/index';
+import {useDispatch} from 'react-redux';
+import {setStatus} from '../actions/tempData';
 
 export const Instance = React.memo(
   ({data, selected, onClick, onDelete, instanceIndex}) => {
-    const [hoverRef, isHovered] = useHover();
-    const [elementToCopy1, doCopy1] = useElementCopy({
-      message: 'Copied drive url',
-    });
-    const [elementToCopy2, doCopy2] = useElementCopy({
-      message: 'Copied drive address',
-    });
-
     const {name, address, isImported} = data;
+    const dispatch = useDispatch();
 
-    let backgroundColor = selected ? primary3 : '#FFF';
+    const backgroundColor = selected ? primary3 : '#FFF';
 
     const styles = {
       outer: {
-        padding: 6,
         backgroundColor: backgroundColor,
         border: `1px solid ${primary3}`,
         borderTop: instanceIndex === 0 ? `1px solid ${primary2}` : 0,
@@ -30,7 +22,8 @@ export const Instance = React.memo(
         borderRight: 0,
         borderLeft: 0,
         color: selected ? '#fff' : primary45,
-        paddingTop: 12,
+        padding: 6,
+        paddingTop: 6,
         fontFamily: 'Open Sans',
         cursor: 'pointer',
         borderRadius: 3,
@@ -50,13 +43,12 @@ export const Instance = React.memo(
       tools: {
         display: 'flex',
         justifyContent: 'flex-end',
-        marginTop: 10,
       },
       toolItem: {
         marginLeft: 10,
       },
       name: {
-        fontSize: 18,
+        fontSize: 16,
         lineHeight: '19px',
         display: 'flex',
         alignItems: 'center',
@@ -70,7 +62,6 @@ export const Instance = React.memo(
       },
     };
 
-    const addressId = `instance-${address}`;
     const shareURL = `${
       window.location.origin + window.location.pathname
     }#/importInstance/${encodeURIComponent(address)}`;
@@ -79,7 +70,6 @@ export const Instance = React.memo(
       <div
         className={'drive'}
         style={styles.outer}
-        ref={hoverRef}
         onClick={(event) => {
           event.stopPropagation();
           onClick();
@@ -97,40 +87,53 @@ export const Instance = React.memo(
               <span style={styles.importedTxt}>[imported]</span>
             ) : null}
           </div>
-          <div id={addressId} style={styles.address}>
-            <span ref={elementToCopy1}>{shareURL}</span>
-            <br />
-            <span ref={elementToCopy2}>{address}</span>
-          </div>
-        </div>
-        <div style={styles.tools}>
-          <div style={styles.toolItem}>
-            <ToolItem
-              defaultColor={selected ? '#fff' : primary45}
-              iconComponent={FiShare2}
-              size={15}
-              changeColor={primary}
-              onClick={doCopy1}
-            />
-          </div>
-          <div style={styles.toolItem}>
-            <ToolItem
-              defaultColor={selected ? '#fff' : primary45}
-              iconComponent={FiCopy}
-              size={15}
-              changeColor={primary}
-              onClick={doCopy2}
-            />
-          </div>
-          <div style={styles.toolItem}>
-            <ToolItem
-              className={'instanceDelete'}
-              defaultColor={selected ? '#fff' : primary45}
-              iconComponent={FiTrash}
-              size={15}
-              changeColor={errorColor}
-              onClick={() => onDelete()}
-            />
+          <div style={styles.tools}>
+            <div style={styles.toolItem}>
+              <ToolItem
+                defaultColor={selected ? '#fff' : primary45}
+                iconComponent={FiShare2}
+                size={15}
+                changeColor={primary}
+                onClick={async () => {
+                  await navigator.clipboard.writeText(shareURL);
+                  dispatch(
+                    setStatus({
+                      message: 'Drive URL copied to clipboard',
+                      isInfo: true,
+                    }),
+                  );
+                  setTimeout(() => dispatch(setStatus({})), 1500);
+                }}
+              />
+            </div>
+            <div style={styles.toolItem}>
+              <ToolItem
+                defaultColor={selected ? '#fff' : primary45}
+                iconComponent={FiCopy}
+                size={15}
+                changeColor={primary}
+                onClick={async () => {
+                  await navigator.clipboard.writeText(address);
+                  dispatch(
+                    setStatus({
+                      message: 'Drive address copied to clipboard',
+                      isInfo: true,
+                    }),
+                  );
+                  setTimeout(() => dispatch(setStatus({})), 1500);
+                }}
+              />
+            </div>
+            <div style={styles.toolItem}>
+              <ToolItem
+                className={'instanceDelete'}
+                defaultColor={selected ? '#fff' : primary45}
+                iconComponent={FiTrash}
+                size={15}
+                changeColor={errorColor}
+                onClick={() => onDelete()}
+              />
+            </div>
           </div>
         </div>
       </div>
