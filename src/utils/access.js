3adee42d5a6582ed3b84ccc89d95cc981a8b9a22
Steve Dakh <slavik0329@gmail.com>
2020-07-20 13:35:28 -0400

    prettier

--- src/utils/access.js
+++ src/utils/access.js
@@ -1,4 +1,3 @@
-
 const perms = {
   admin: 'admin',
   write: 'write',
@@ -6,19 +5,19 @@ const perms = {
 
 export function localUserId(sharedFS) {
   return sharedFS._db.identity.id;
-};
+}
 
 export function writers(sharedFS) {
   return sharedFS.access.get(perms.write);
-};
+}
 
 export function admin(sharedFS) {
   return sharedFS.access.get(perms.admin);
-};
+}
 
 export async function grantWrite(sharedFS, userId) {
   return sharedFS.access.grant(perms.write, userId);
-};
+}
 
 export function hasWrite(sharedFS, userId = localUserId(sharedFS)) {
   return new Set([...writers(sharedFS), ...admin(sharedFS)]).has(userId);
