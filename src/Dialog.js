0366d0538b1cf8c5c2e8cb36fd7ee919e8ec1179
Steve Dakh <slavik0329@gmail.com>
2020-07-21 18:19:17 -0400

    fix dialog in mobile

--- src/Dialog.js
+++ src/Dialog.js
@@ -1,50 +1,53 @@
 import React from 'react';
 import {primary45} from './colors';
 import {FaTimes} from 'react-icons/fa';
-
-const styles = {
-  container: {
-    position: 'absolute',
-    backgroundColor: '#FFF',
-    border: `1px solid ${primary45}`,
-    borderRadius: 4,
-    top: 150,
-    left: '50%',
-    transform: 'translateX(-50%)',
-    width: '80%',
-    fontFamily: 'Open Sans',
-    zIndex: 10000,
-    boxShadow: '0 0px 14px hsla(0, 0%, 0%, 0.2)',
-  },
-  header: {
-    backgroundColor: primary45,
-    color: '#FFF',
-    padding: 8,
-    fontSize: 14,
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  body: {
-    padding: 14,
-  },
-  xIcon: {
-    cursor: 'pointer',
-  },
-  background: {
-    position: 'fixed',
-    width: '100%',
-    height: '100%',
-    top: 0,
-    left: 0,
-    backgroundColor: '#00000033',
-  },
-};
+import {useIsMobile} from './hooks/useIsMobile';
 
 export function Dialog({isVisible, body, onClose, title}) {
+  const isMobile = useIsMobile();
+
   if (!isVisible) {
     return null;
   }
+
+  const styles = {
+    container: {
+      position: isMobile ? 'fixed' : 'absolute',
+      backgroundColor: '#FFF',
+      border: `1px solid ${primary45}`,
+      borderRadius: 4,
+      top: 150,
+      left: '50%',
+      transform: 'translateX(-50%)',
+      width: '80%',
+      fontFamily: 'Open Sans',
+      zIndex: 10000,
+      boxShadow: '0 0px 14px hsla(0, 0%, 0%, 0.2)',
+    },
+    header: {
+      backgroundColor: primary45,
+      color: '#FFF',
+      padding: 8,
+      fontSize: 14,
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    body: {
+      padding: 14,
+    },
+    xIcon: {
+      cursor: 'pointer',
+    },
+    background: {
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      backgroundColor: '#00000033',
+    },
+  };
 
   return (
     <div style={styles.outer}>
