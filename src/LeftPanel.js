ced8110091ccd66d843b50dfba405cf7cef7deb2
Steve Dakh <slavik0329@gmail.com>
2020-07-23 17:12:26 -0400

    nicer mobile header

--- src/LeftPanel.js
+++ src/LeftPanel.js
@@ -13,82 +13,81 @@ import {useIsSmallScreen} from './hooks/useIsSmallScreen';
 import {FaGithub, FaHome} from 'react-icons/fa/index';
 import logo from './logo.png';
 
-const styles = {
-  container: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    backgroundColor: primary45,
-    color: '#FFF',
-    padding: '20px 10px',
-    minWidth: 200,
-    fontFamily: 'Open Sans',
-    paddingBottom: 0,
-  },
-  logo: {
-    display: 'inline-block',
-    fontFamily: 'MuseoModerno',
-    color: '#FFF',
-    fontSize: 24,
-    fontWeight: 400,
-    marginBottom: 20,
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    userSelect: 'none',
-    cursor: 'pointer',
-    lineHeight: '24px',
-  },
-  settingsBlock: {
-    bottom: 0,
-    width: '100%',
-  },
-  mobilePadding: {
-    paddingBottom: 6,
-  },
-  menuIcon: {
-    position: 'absolute',
-    top: 25,
-    left: 14,
-  },
-  logoContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  footer: {
-    textAlign: 'center',
-    marginBottom: 8,
-    fontSize: 13,
-    lineHeight: '14px',
-    fontFamily: 'Open Sans',
-  },
-  iconGithub: {
-    cursor: 'pointer',
-  },
-  githubTxt: {
-    fontSize: 10,
-    userSelect: 'none',
-  },
-  icon: {
-    position: 'relative',
-    top: 6,
-    width: 30,
-  },
-  logoTitle: {
-    display: 'inline-flex',
-  },
-};
-
 export function LeftPanel({
   setCurrentRightPanel,
   currentRightPanel,
   isDownload,
 }) {
   const isSmallScreen = useIsSmallScreen();
-
   const [isMobileOpen, setIsMobileOpen] = useState(false);
+
+  const styles = {
+    container: {
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      backgroundColor: primary45,
+      color: '#FFF',
+      padding: `${isSmallScreen ? 8 : 20}px 10px 0px 10px`,
+      minWidth: 200,
+      fontFamily: 'Open Sans',
+      paddingBottom: 0,
+    },
+    logo: {
+      display: 'inline-block',
+      fontFamily: 'MuseoModerno',
+      color: '#FFF',
+      fontSize: 24,
+      fontWeight: 400,
+      marginBottom: isSmallScreen && !isMobileOpen ? 8 : 20,
+      textAlign: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+      userSelect: 'none',
+      cursor: 'pointer',
+      lineHeight: '24px',
+    },
+    settingsBlock: {
+      bottom: 0,
+      width: '100%',
+    },
+    mobilePadding: {
+      paddingBottom: 6,
+    },
+    menuIcon: {
+      position: 'absolute',
+      top: isSmallScreen ? 16 : 25,
+      left: 14,
+    },
+    logoContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    footer: {
+      textAlign: 'center',
+      marginBottom: 8,
+      fontSize: 13,
+      lineHeight: '14px',
+      fontFamily: 'Open Sans',
+    },
+    iconGithub: {
+      cursor: 'pointer',
+    },
+    githubTxt: {
+      fontSize: 10,
+      userSelect: 'none',
+    },
+    icon: {
+      position: 'relative',
+      top: 6,
+      width: 30,
+    },
+    logoTitle: {
+      display: 'inline-flex',
+    },
+  };
 
   let iconComponent = FaBars;
   if (isMobileOpen) {
@@ -118,10 +117,7 @@ export function LeftPanel({
                 window.location.origin + window.location.pathname
               }`;
             }}>
-            <img
-              src={logo}
-              style={styles.icon}
-            />
+            <img src={logo} style={styles.icon} />
             <div style={styles.logoTitle}>Sailplane</div>
           </div>
         </div>
